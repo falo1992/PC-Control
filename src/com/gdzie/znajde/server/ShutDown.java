a73610eb167cb1d17ebc32fc0bc35d620a5b04ba
Olaf Majer <olaf@eboss.pl>
2017-05-25 02:14:40 +0200

    Osobna klasa na wyłączanie

--- src/com/gdzie/znajde/server/ShutDown.java
+++ src/com/gdzie/znajde/server/ShutDown.java
@@ -4,6 +4,7 @@ import com.sun.jna.Native;
 
 public class ShutDown {
 	public static boolean shutDown(){
+		
 		IUser32 user32 = (IUser32) Native.loadLibrary("user32",IUser32.class);
 		boolean restart = user32.ExitWindowsEx(0x00000008, 0x80000000);
 		return restart;
