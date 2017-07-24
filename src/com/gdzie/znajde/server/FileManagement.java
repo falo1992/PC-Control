be91b6e482b36135617e5493839deac28736fe7f
Olaf Majer <olaf@eboss.pl>
2017-07-25 00:03:03 +0200

    Listowanie katalogów, upload i download plikow

--- src/com/gdzie/znajde/server/FileManagement.java
+++ src/com/gdzie/znajde/server/FileManagement.java
@@ -1,11 +1,19 @@
 package com.gdzie.znajde.server;
 
 import java.io.*;
+import java.net.Socket;
 
-public class FileMenagement {
+public class FileManagement {
 	
-	private static String previousPath;
-	private static String currentPath;
+	private static Socket socket = null;
+	FileManagement(Socket socket){
+		FileManagement.socket = socket;
+	}
+	
+	private static byte[] buffer = new byte[8192];
+	private static String previousPath = new String();
+	private static String currentPath = new String();
+	private static String user = System.getProperty("user.name");
 	
 	public static String listFolder(String path){
 		currentPath = new String(path);
@@ -14,9 +22,9 @@ public class FileMenagement {
 		StringBuilder result = new StringBuilder();
 		for(File file: folderList){
 			if(file.isDirectory()){
-				result.append("Folder "+file.getName()+"\n");
+				result.append("Folder\t"+file.getName()+"\n");
 			}else{
-				result.append("Plik "+file.getName()+"\n");
+				result.append("Plik\t"+file.getName()+"\n");
 			}
 		}
 		
@@ -24,8 +32,8 @@ public class FileMenagement {
 	}
 	
 	public static String goToFolder(String folder){
-		previousPath = new String(currentPath);
-		currentPath = new String(currentPath+"\\"+folder);
+		previousPath = currentPath;
+		currentPath = currentPath+"\\"+folder;
 		
 		return listFolder(currentPath);
 	}
@@ -34,11 +42,39 @@ public class FileMenagement {
 		return listFolder(previousPath);
 	}
 	
-	public static boolean downloadFile(){
+	public static boolean downloadFile(String filePath) {
+		try{
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream(filePath));
+			BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+			int count = 0;
+			while( (count = in.read(buffer)) > 0 ) {
+				out.write(buffer, 0, count);
+			}
+			in.close();
+			out.flush();
+			out.close();
+			return true;
+		}catch (IOException e){
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
-	public static boolean uploadFile(){
+	public static boolean uploadFile(String filePath){
+		try{
+			BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filePath));
+			int count = 0;
+			while( (count = in.read(buffer)) > 0){
+				out.write(buffer, 0, count);
+			}
+			in.close();
+			out.flush();
+			out.close();
+			return true;
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
@@ -54,5 +90,6 @@ public class FileMenagement {
 		System.out.print(listFolder("."));
 		System.out.print(goToFolder(".settings"));
 		System.out.print(goToPrevious());
+		System.out.println(user);
 	}
 }
