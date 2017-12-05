29b8098b884d4c9861abf1005ef7768ccc901b33
Olaf Majer <olaf@eboss.pl>
2017-12-05 01:07:35 +0100

    Adding server icon to system tray

---
+++ src/com/gdzie/znajde/server/gui/AboutFrame.java
@@ -0,0 +1,53 @@
+package com.gdzie.znajde.server.gui;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class AboutFrame extends JFrame {
+	
+	private JLabel textLabel = new JLabel();
+	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private Scanner scanner;
+
+	public AboutFrame() {
+		this(500,300);
+	}
+	
+	public AboutFrame(int x, int y) {
+		super("About PC-Control");
+		try {
+			scanner = new Scanner(new File("doc/About.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent windowEvent) {
+				AboutFrame.this.setVisible(false);
+			}
+		});
+		setPreferredSize(new Dimension(x, y));
+		add(textLabel);
+		pack();
+		setLocationRelativeTo(null);
+		readFile();
+		setVisible(false);
+		setLocation(screenSize.width/2 - this.getSize().width/2, screenSize.height/2 - this.getSize().height/2);
+	}
+	
+	private void readFile() {
+		while(scanner.hasNextLine()) {
+			textLabel.setText(textLabel.getText() + scanner.nextLine());
+		}
+	}
+}
--- src/com/gdzie/znajde/server/gui/ServerFrame.java
+++ src/com/gdzie/znajde/server/gui/ServerFrame.java
@@ -1,26 +1,20 @@
 package com.gdzie.znajde.server.gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
+import javax.swing.*;
+import javax.swing.border.*;
 
 import com.gdzie.znajde.server.VolumeManagement;
 
 @SuppressWarnings("serial")
 public class ServerFrame extends JFrame {
-	static private JTextArea textArea = new JTextArea(14,40);
-	static private JScrollPane scrollPane = new JScrollPane();
-	static private JPanel panel = new JPanel();
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private static JTextArea textArea = new JTextArea(14,40);
+	private static JScrollPane scrollPane = new JScrollPane();
+	private static JPanel panel = new JPanel();
+	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private static JFrame aboutFrame = new AboutFrame();
 	
 	public ServerFrame() {
 		this(500,300);
@@ -32,7 +26,7 @@ public class ServerFrame extends JFrame {
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent ev) {
 				VolumeManagement.removeProgram();
-				System.exit(0);
+				ServerFrame.this.setVisible(false);;
 			}
 		});
 		setPreferredSize(new Dimension(x, y));
@@ -45,7 +39,79 @@ public class ServerFrame extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setLocation(screenSize.width/2 - this.getSize().width/2, screenSize.height/2 - this.getSize().height/2);
+		initTray();
+	}
+	
+	public void initTray() {
+		Image image = Toolkit.getDefaultToolkit().getImage("icon\\icon.jpg");
+		TrayIcon trayIcon = new TrayIcon(image);
+		SystemTray tray = SystemTray.getSystemTray();
+		PopupMenu popup = new PopupMenu();
 		
+		MenuItem aboutItem = new MenuItem("About");
+        MenuItem showWindow = new MenuItem("Show");
+        MenuItem exitItem = new MenuItem("Exit");
+		
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(showWindow);
+        popup.addSeparator();
+        popup.add(exitItem);
+        
+        aboutItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				aboutFrame.setVisible(true);
+			}
+		});
+        
+        exitItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+        
+        showWindow.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ServerFrame.this.setVisible(true);
+			}
+		});
+        
+        trayIcon.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if(e.getClickCount() == 2) {
+					ServerFrame.this.setVisible(true);
+				}
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {}
+		});
+        
+        trayIcon.setPopupMenu(popup);
+        
+		try {
+			tray.add(trayIcon);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void log(String msg) {
